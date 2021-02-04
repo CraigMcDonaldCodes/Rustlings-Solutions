3ff332b2fc30b4f5d1969ef6c33bee73e157a895
Craig A. McDonald <craig@mcdonald.codes>
2021-02-04 19:50:50 +0000

    Primitive_types1 completed

---
+++ exercises/primitive_types/primitive_types1.rs
@@ -0,0 +1,20 @@
+// primitive_types1.rs
+// Fill in the rest of the line that has code missing!
+// No hints, there's no tricks, just get used to typing these :)
+
+fn main() {
+
+    // Stating the type
+    let is_morning: bool = true;
+
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    // Inferred type
+    let is_evening = false;
+
+    if is_evening {
+        println!("Good evening!");
+    }
+}
